965a32bd991d0f1ae260c469531db417c06ba111
Kyoya Takumu <kyoyatakumu@gmail.com>
2026-06-12 01:45:28 +0900

    feat: migrationファイルの追加

---
+++ laravel-back/database/migrations/2026_06_08_045211_create_reactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('article_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['like', 'bookmark']);
+            $table->timestamp('created_at')->nullable();
+            $table->primary(['user_id', 'article_id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
